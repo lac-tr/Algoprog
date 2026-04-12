c1fd3f0e6a43e2b65c5ca66a11cb8978796b2b63
lac-tr <intandewati@gmail.com>
2026-04-12 20:12:12 +0800

    Add code

---
+++ Responsi/Minggu 2/probA.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\"Kampus Terbaik\"\n");
+    printf("Kampus terbaik, inovasi wujudkan impian\n");
+    printf("Kampus terbaik, integritas kuatkan tujuan\n");
+    printf("Kampus terbaik beri terbaik untuk negeri,\n");
+    printf("Untuk dunia\n");
+    printf("Umat manusia dan alam semesta\n");
+    return 0;
+}
