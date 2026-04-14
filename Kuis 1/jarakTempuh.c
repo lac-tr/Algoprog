b36e6e0a8c762471307a9dea8736dd49e90214a3
lac-tr <intandewati@gmail.com>
2026-04-14 13:05:08 +0800

    Add code

---
+++ Kuis 1/jarakTempuh.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, kec, jarak, kj;
+    int temp = 0;
+    int sum = 0;
+    int sumj = 0;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++){ 
+        scanf("%d %d", &kec, &jarak);
+        sumj = jarak - temp;
+        temp += sumj;
+        // printf("%d\n", sumj);
+        kj = kec * sumj;
+        sum = kj + sum;
+        // printf("%d\n", sum);
+    }
+
+    printf ("\n%d\n", sum);
+
+    return 0;
+}
